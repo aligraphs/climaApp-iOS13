f2371975a795505c4979a2b99af159a4b880fc01
Ali Ünal <unal.ali.95@gmail.com>
2020-12-09 10:18:50 +0300

    Last Commit

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Ali Ünal on 28/11/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+}
